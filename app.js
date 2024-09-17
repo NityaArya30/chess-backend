03c496db7262eb96837df54a42b40122ee31bbe9
Nitya Arya <nityaarya11@gmail.com>
2024-09-17 17:38:29 +0530

    Chess Board backend Added

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const io = socket(server);
 
 const chess = new Chess();
 let players = {};
-let currentPlayer = "W";
+let currentPlayer = "w";
 
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));
@@ -23,11 +23,56 @@ app.get("/", (req, res) => {
 io.on("connection", function(uniquesocket) {
     console.log("connected");
 
-    //receiving on backend
-    uniquesocket.on("masala", function() {
-        console.log("masala received");
-        //sending to frontend chessgame.js
-        io.emit("churan papdi");
+    // //receiving on backend
+    // uniquesocket.on("masala", function() {
+    //     console.log("masala received");
+    //     //sending to frontend chessgame.js
+    //     io.emit("churan papdi");
+    // })
+
+    // uniquesocket.on("disconnected", function() {
+    //     console.log("disconnected"); 
+    // });
+
+    if(!players.white) {
+        players.white = uniquesocket.id;
+        uniquesocket.emit("playerRole", "w");
+    } else if(!players.black){
+        players.black = uniquesocket.id;
+        uniquesocket.emit("playersRole", "b");
+    } else {
+        uniquesocket.emit("spectatorRole");
+    }
+
+    //game chdkr gaya
+    uniquesocket.on("disconnect", function() {
+        if(uniquesocket.id === players.white) {
+            delete players.white;
+        }else if(uniquesocket.id === players.black) {
+            delete players.black;
+        }
+    });
+    uniquesocket.on("move", (move) => {
+        try{
+            //black ki turn h to white chalega to return krna aur vice versa
+            if(chess.turn()==='w' && uniquesocket.id !== players.white) return; 
+            if(chess.turn()==='b' && uniquesocket.id !== players.black) return; 
+
+            const result = chess.move(move);
+            if(result) {
+                currentPlayer = chess.turn();
+                //sbko bhj rhe hein jo move hua hai
+                io.emit("move", move);
+                //fen board ki current state deta hai
+                io.emit("boardState", chess.fen())
+            } else {
+                console.log("Invalid Move : ", move);
+                uniquesocket.emit("invalid move : ", move);
+            }
+        } catch(err) {
+            console.log(err);
+            uniquesocket.emit("Invalide move : ", move);
+        }
     })
 });
 
--- public/js/chessgame.js
+++ public/js/chessgame.js
@@ -1,7 +1,58 @@
 const socket = io();
 //sent to backend
-socket.emit("masala");
-socket.on("churan papdi", function() {
-    //received from backend
-    console.log("Churan papdi received");
-});
+// socket.emit("masala");
+// socket.on("churan papdi", function() {
+//     //received from backend
+//     console.log("Churan papdi received");
+//});
+const chess = new Chess();
+const boardElement = document.querySelector(".chessboard");
+let draggedPiece = null;
+let sourceSquare = null;
+let playerRole = null;
+
+const renderBoard = ()=> {
+    const board = chess.board();
+    boardElement.innerHTML = "";
+    //console.log(board);
+    board.forEach((row, rowindex) => {
+        //console.log(row);
+        row.forEach((square, squareindex) => {
+            //console.log(square);
+            const squareElement = document.createElement("div");
+            //check box bnane k liye light aur dark
+            squareElement.classList.add("square", 
+                (rowindex+ squareindex)%2 === 0 ? "light" : "dark"
+            );
+            squareElement.dataset.row = rowindex;
+            squareElement.dataset.col = squareindex;
+
+            //checkbox p element h ya ni
+            if(square) {
+                const pieceElement = document.createElement("div");
+                pieceElement.classList.add(
+                    "piece", 
+                    square.color === 'w' ? "white" : "black"
+                );
+                pieceElement.innerText = "";
+                pieceElement.draggable = playerRole === square.color;
+                pieceElement.addEventListener("dragstart", () => {
+                    if(pieceElement.draggable) {
+                        draggedPiece = pieceElement;
+                        sourceSquare = {row: rowindex, col: squareindex};
+                    }
+                });
+            }
+        });
+    });
+}
+
+const handleMove = () => {
+
+}
+
+const getPieceUnicode = () => {
+
+}
+
+renderBoard();
